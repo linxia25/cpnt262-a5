50039146dd0e2e8cc54ad451b0e477a560b3b5a4
linxia25 <linxia25@hotmail.com>
2021-03-25 15:46:40 -0600

    updata

--- public/js/client.js
+++ public/js/client.js
@@ -16,9 +16,9 @@ furnitures.forEach(function(furnitures) {
   
 output +=
     `<figure>
-      <img src="${furnitures.pathURL}" alt="pexels:${furnitures.title}" width='${furnitures.width}px' height='${furnitures.height}px'>
+    <a href="furnitures/${furnitures.title}" target:"_blank"><img src="${furnitures.pathURL}" alt="pexels:${furnitures.title}" width='${furnitures.width}px' height='${furnitures.height}px'></a>
       <figcaption>
-        <h1>${furnitures.title}</h1>
+        <h1> <a href="furnitures/${furnitures.title}" target:"_blank">${furnitures.title}</a></h1>
         <h2>${furnitures.description}</h2>
         <a href="${furnitures.creditURL}" target="_blank">Design by ${furnitures.credit}</a>
         <a href="${furnitures.linkURL}" target="_blank">More Info</a>
